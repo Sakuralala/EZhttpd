5e1e52716d16ed33890012b3e9bb9e27f708eeaf
oldhen <343541994@qq.com>
2019-01-10 18:35:32 +0800

    async log completed initially.

--- bases/asyncLog.h
+++ bases/asyncLog.h
@@ -11,26 +11,49 @@
 #include "mutex.h"
 #include "countdownLatch.h"
 #include "condition.h"
-
+#include "buffer.h"
 namespace bases
 {
 class AsyncLog
 {
-  public:
-  AsyncLog():thread_(std::bind(&AsyncLog::writeToFile,this),"Log thread"),latch_(1),mutex_(),cond_(mutex_){}
-    void run();
-    void stop();
-    //前端写消息到缓冲区
-    void append();
-    //后端把缓冲区中的内容写入文件
-    void writeToFile();
 
-  private:
-    Thread thread_;
-    //让日志线程能够开始执行
-    CountdownLatch latch_;
-    Mutex mutex_;
-    Condition cond_;
+public:
+  typedef FixedLengthBuffer<LargeBufferLength> LogBuffer;
+  typedef std::unique_ptr<LogBuffer> LogBufferPtr;
+  typedef std::vector<LogBufferPtr> BufferVector;
+  AsyncLog();
+  ~AsyncLog();
+  void run();
+  void stop();
+  //前端写消息到缓冲区
+  bool append(const char *message, size_t length);
+  //后端把缓冲区中的内容写入文件
+  void writeToFile();
+
+private:
+  Thread thread_;
+  //让日志线程能够开始执行
+  CountdownLatch latch_;
+  Mutex mutex_;
+  //用于定时将缓冲区中的日志写入到文件中
+  Condition cond_;
+  bool looping_;
+  LogBufferPtr current_;
+  //流动式缓冲区的设计:
+  //总的缓冲区的个数在一开始就确定了，不可更改;
+  //存在三个队列，一个是空buffer队列empty_，另一个是满buffer队列full_，这两个前后端都可以访问；
+  //最后一个队列为满buffer队列的copy:fullCopy,只由后端使用，作用是快速和满buffer队列进行交换从而减小临界区(两个队列的容量是一样的)；
+  //大致的工作流程如下:
+  //前端只往current_中写数据，满了就将其加入到full_中，并尝试从empty_中拿一块新的，没有则直接返回;
+  //后端将fullCopy和full_进行交换，然后就可以离开临界区(然后前端就可以继续写了),开始将fullCopy(实际上是full_)中的数据写入文件，写入完成之后再次进入临界区，这次是为了把fullCopy中的buffer加入到empty中，以便循环利用
+  //按上述方式，大概就形成了一个buffer流动的循环
+  //备用的buffer块
+  BufferVector empty_;
+  //写满了的buffer块
+  BufferVector full_;
+  //满的buffer块和备用buffer块之和的最大值
+  static const int MaxNumber=16;
+  static const int DefaultWriteSeconds = 5;
 };
 } // namespace bases
 #endif // !__asynclog_h
